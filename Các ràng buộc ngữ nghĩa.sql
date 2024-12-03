3e60662d531ad73052dc3fec1c57274e8c50fd7e
NLDenn <165903358+NLDenn@users.noreply.github.com>
2024-12-03 14:46:07 +0700

    Update Các ràng buộc ngữ nghĩa.sql

--- Các ràng buộc ngữ nghĩa.sql
+++ Các ràng buộc ngữ nghĩa.sql
@@ -34,3 +34,41 @@ BEGIN
 END$$
 DELIMITER ;
 #######################################################################################
+-- khong quan li chinh minh
+drop trigger if exists k_quanli_chinhminh_insert;
+delimiter //
+create trigger k_quanli_chinhminh_insert
+before insert 
+on nvchinhthuc
+for each row
+begin 
+IF new.msnv =new.nguoiquanly THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ma nguoi quan ly khong the trung voi ma nhan vien';
+    END IF;
+end //
+delimiter ;
+drop trigger if exists k_quanli_chinhminh_update;
+delimiter //
+create trigger k_quanli_chinhminh_update
+before update
+on nvchinhthuc
+for each row
+begin 
+IF new.msnv =new.nguoiquanly THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ma nguoi quan ly khong the trung voi ma nhan vien';
+    END IF;
+end //
+delimiter ;
+#################################################################################################
+-- khong cho cap nhat khi gio ra khac gio vao
+drop trigger if exists vao_bang_ra;
+delimiter ??
+create trigger vao_bang_ra
+before update 
+on ngaylamviec for each row
+begin
+if(not(old.giovao=old.giora)) then 
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Khong the cap nhat ngay lam viec';
+end if;
+end ??
+delimiter ;
