ba6046f6b95104249888f50cb3438ede5fe4fc66
Cong Phuong <bonanhphuong@gmail.com>
2024-12-09 03:53:06 +0700

    1.2.3 phương update

--- 1.2.3.sql
+++ 1.2.3.sql
@@ -1,5 +1,5 @@
 
--- 1 Tìm tên , luong thuc te của nhân viên có lương thực tế cao nhất trong tháng a va nam b cua phong ban c co luong thuc te > d
+-- 1 Tìm tên , luong thuc te của nhân viên có lương thực tế cao nhất trong tháng a va nam b cua phong ban c co luong thuc te < d
 drop PROCEDURE if exists tim_maxluong_withinPhongban_inMonth ;
 DELIMITER //
 CREATE PROCEDURE tim_maxluong_withinPhongban_inMonth ( t int , n int  , d dec(10,2) )
@@ -12,8 +12,8 @@ BEGIN
         group by nv.mspb
     ) as m , nhanvien as nv, bangluong as bl2 
     where   nv.mspb = m.mspb and bl2.luongthucte = m.maxluong and nv.msnv = bl2.msnv
-    GROUP BY nv.hoten, nv.msnv, bl2.luongthucte
-    HAVING bl2.luongthucte >  d 
+    GROUP BY nv.hoten, nv.msnv, bl2.luongthucte -- nên bỏ
+    HAVING bl2.luongthucte <  d -- nên sửa
     ORDER BY bl2.luongthucte;
 END // 
 DELIMITER ;
@@ -21,36 +21,30 @@ DELIMITER ;
 select * from bangluong;
 call tim_maxluong_withinPhongban_inMonth(1,2000 ,99999999.99);
 ##############################################################################
+#######################################
 -- Hiển thị tổng số giờ làm thêm và tổng số lương làm thêm mà công ty đã chi trả trong 
 -- mọi tháng của năm input_year sắp xếp giảm dần theo tổng lương chi trả từng tháng
 drop procedure if exists SUM_lam_them;
 DELIMITER $$
-create procedure SUM_lam_them(
-    input_year int
-)
-begin
+CREATE PROCEDURE SUM_lam_them(input_year int) 
+BEGIN 
 	DECLARE year_temp YEAR;
-	set year_temp = input_year;
-    
-	select L.thang,
-		   SUM(L.luonglamthem) as tong_luong_lam_them,
-           SUM(C.sogiolamthem) as tong_gio_lam_them
-    from 
-		bangluong L
-    join
-		bangchamcong C
-	on
-		L.msnv = C.msnv and L.thang = C.thang and L.nam = C.nam 
-    where    
-        L.nam = input_year
-    group by 
-		L.thang
-	order by tong_luong_lam_them desc;
-end
+	SET year_temp = input_year;
+	SELECT L.thang,
+		   SUM(L.luonglamthem) AS tong_luong_lam_them,
+		   SUM(C.sogiolamthem) AS tong_gio_lam_them
+	FROM bangluong L
+	JOIN bangchamcong C ON L.msnv = C.msnv
+		AND L.thang = C.thang
+		AND L.nam = C.nam
+	WHERE L.nam = input_year
+	GROUP BY L.thang
+	ORDER BY tong_luong_lam_them DESC; 
+END
 $$
 DELIMITER ;
 -- test
-call SUM_lam_them(2024);
+CALL SUM_lam_them(2024);
 ##############################################################################
 
 -- ############################################################
@@ -78,3 +72,28 @@ BEGIN
 END //
 
 DELIMITER ;
+
+##############################
+-- new verse
+drop PROCEDURE if exists tim_maxluong_withinPhongban_inMonth ;
+DELIMITER //
+CREATE PROCEDURE tim_maxluong_withinPhongban_inMonth ( t int , n int  , d dec(10,2) )
+BEGIN
+    select distinct nv.hoten, nv.msnv , bl2.luongthucte, nv.mspb
+    from (
+        select max(T.luongthucte) as maxluong, T.masophongban as mspb
+		from 	(select bl.luongthucte as luongthucte, nv.mspb as masophongban 
+				from bangluong as bl, nhanvien as nv
+				where bl.thang =  t and bl.nam =  n and nv.msnv = bl.msnv
+				group by bl.luongthucte, masophongban
+				having bl.luongthucte <  d) as T
+		group by T.masophongban
+    ) as m , nhanvien as nv, bangluong as bl2 
+    where   nv.mspb = m.mspb and bl2.luongthucte = m.maxluong and nv.msnv = bl2.msnv
+    ORDER BY bl2.luongthucte;
+END // 
+DELIMITER ;
+
+
+CALL tim_maxluong_withinPhongban_inMonth(6,2024 ,10.00);
+select * from bangluong;
